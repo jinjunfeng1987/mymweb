cb6c84ae766c413fc7090a4be98f322d051a3046
jinjunfeng1987 <380726783@qq.com>
2018-04-16 19:36:58 +0800

    分类列表数据渲染功能完成

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
+// 设置图片基准路径
+const APP = {
+  imgBaseUrl : 'http://47.96.21.88:8888/'
+}
+
 // 设置请求的基准路径
 axios.defaults.baseURL = 'http://47.96.21.88:8888/api/public/v1';
+// axios.defaults.baseURL = 'http://localhost:8888/api/public/v1'; 
 // 响应拦截器
 axios.interceptors.response.use(function (response) {
     // 在我们得到服务器返回的数据之前做一些处理
